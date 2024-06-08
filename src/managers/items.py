1664d1e4cd9d87889aa7cadfb21b3e8eda14b49c
MaxGdr <gendre.maxime@gmail.com>
2024-06-08 19:29:09 +0200

    simple get items

--- src/managers/items.py
+++ src/managers/items.py
@@ -15,9 +15,7 @@ class ItemsManager:
     async def get(
         self, skip: int, limit: int, user_id: int | None = None
     ) -> List[ItemSchema]:
-        db_items: List[Item] = await self._items_crud.get_items(
-            skip=skip, limit=limit
-        )
+        db_items: List[Item] = await self._items_crud.get_items(skip=skip, limit=limit)
         return [ItemSchema._from_dto(item=item) for item in db_items]
 
     async def create(self, item: ItemSchema) -> ItemSchema:
